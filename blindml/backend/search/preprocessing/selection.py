ee0c7cd8b2dbe4c5ed9f27d2c5c3a7cb3a5ebdb9
Ben Clifford <benc@hawaga.org.uk>
2021-01-19 12:24:37 +0000

    Disable CUDA because my laptop has no CUDA
    Experiments still seem to run but a later stage fails with an error:
    
    ~/cqx/c/datastation/virtualenv/lib/python3.7/site-packages/torch/cuda/__init__.py in _lazy_init()
        170         # This function throws if there's a driver initialization error, no GPUs
        171         # are found or any other error occurs
    --> 172         torch._C._cuda_init()
        173         # Some of the queued calls may reentrantly call _lazy_init();
        174         # we need to just return without initializing in that case.
    
    RuntimeError: Found no NVIDIA driver on your system. Please check that you have an NVIDIA GPU and installed a driver from http://www.nvidia.com/Download/index.aspx

--- blindml/backend/search/preprocessing/selection.py
+++ blindml/backend/search/preprocessing/selection.py
@@ -1,16 +1,24 @@
 from nni.feature_engineering.gradient_selector import FeatureGradientSelector
 import numpy as np
+import torch
 
 def select_features(X_train, y_train):
-    try:
-        fgs = FeatureGradientSelector(n_epochs=10, device="cuda")
-        fgs.fit(X_train, y_train)
-        feat_idxs = fgs.get_selected_features()
-    except AssertionError as a:
-        assert a.args[0] == 'Torch not compiled with CUDA enabled'
+    # Prefer to run with CUDA but fall back if it detects that that will
+    # not work - there are two ways this detection is done: if the code
+    # raises an AssertionError coming from torch not supporting CUDA,
+    # and from Torch supporting CUDA but CUDA not being available on the
+    # executing system.
+    if torch.cuda.is_available():
+        try:
+            fgs = FeatureGradientSelector(n_epochs=10, device="cuda")
+        except AssertionError as a:
+            assert a.args[0] == 'Torch not compiled with CUDA enabled'
+            fgs = FeatureGradientSelector(n_epochs=10, device="cpu")
+    else:
         fgs = FeatureGradientSelector(n_epochs=10, device="cpu")
-        fgs.fit(X_train, y_train)
-        feat_idxs = fgs.get_selected_features()
+
+    fgs.fit(X_train, y_train)
+    feat_idxs = fgs.get_selected_features()
 
     if not feat_idxs:
         _rows, cols = X_train.shape
