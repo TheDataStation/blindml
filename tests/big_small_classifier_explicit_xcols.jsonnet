c7b8b9dadd4b00e6c695f1b1dc1b6ae638f7700c
Ben Clifford <benc@hawaga.org.uk>
2021-04-09 08:49:53 +0000

    Fix broken explicit-X_cols codepath and add a test

---
+++ tests/big_small_classifier_explicit_xcols.jsonnet
@@ -0,0 +1,21 @@
+{
+  user: 'max',
+  access_token: '1234567',
+  task: {
+    type: 'classification',
+    search_time: 60,
+    payload: {
+      y_col: 'human_size',
+      X_cols: ['int_size'],
+      data_path: "./tests/big_small_classifier.csv"
+    },
+  },
+  dos: {
+    metric: 'accuracy',
+    range: [0.8, 1],
+  },
+  trust_constraints: {
+    freshness: 'last_week',
+    user: 'all_groups',
+  },
+}
--- tests/test_big_small_classifier.py
+++ tests/test_big_small_classifier.py
@@ -28,6 +28,30 @@ def test_big_small_classifier_quick():
 
     print(f"test finished with accuracy {accuracy}")
 
+def test_big_small_classifier_explicit_X_cols():
+    """Same test as above, but with a config that uses explicit X cols,
+    to test that configuration code path"""
+    task = parse_task_capsule("tests/big_small_classifier_explicit_xcols.jsonnet")
+    task.search_for_model()
+
+    model = task._auto_sk_model
+
+    X_train, y_train = task._data_set.get_train_data()
+
+    y_pred = model.predict(X_train)
+
+    accuracy = accuracy_score(y_train, y_pred)
+
+    # on such a tiny dataset, this should predict with complete
+    # accuracy on the training data
+    threshold_accuracy = 1.0
+
+    if accuracy < threshold_accuracy:
+        raise RuntimeError(f"Classifier accuracy {accuracy} was below reasonable threshold {threshold_accuracy}")
+
+    print(f"test finished with accuracy {accuracy}")
+
+
 
 if __name__ == "__main__":
     test_big_small_classifier_quick()
